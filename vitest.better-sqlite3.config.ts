338796c6f5fb7de3bd36d3a879ebd7447b581b9a
Meno Abels <meno.abels@adviser.com>
2024-09-03 15:46:21 +0200

    chore: exclude configtest

--- vitest.better-sqlite3.config.ts
+++ vitest.better-sqlite3.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     name: "better-sqlite3",
-    exclude: ["node_modules/@fireproof/core/tests/react/**"],
+    exclude: [
+	"node_modules/@fireproof/core/tests/react/**",
+	"node_modules/@fireproof/core/tests/fireproof/config.test.ts"
+    ],
     include: ["src/store-sql/**/*test.?(c|m)[jt]s?(x)", "node_modules/@fireproof/core/tests/**/*test.?(c|m)[jt]s?(x)"],
     globals: true,
     setupFiles: "./setup.better-sqlite3.ts",
--- vitest.node-sqlite3-wasm.config.ts
+++ vitest.node-sqlite3-wasm.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     name: "node-sqlite3-wasm",
-    exclude: ["node_modules/@fireproof/core/tests/react/**"],
+    exclude: [
+	"node_modules/@fireproof/core/tests/react/**",
+	"node_modules/@fireproof/core/tests/fireproof/config.test.ts"
+    ],
     include: ["src/store-sql/**/*test.?(c|m)[jt]s?(x)", "node_modules/@fireproof/core/tests/**/*test.?(c|m)[jt]s?(x)"],
     globals: true,
     setupFiles: "./setup.node-sqlite3-wasm.ts",
