70136676eca9d14905d429c364dde2677f0be9f1
Marty Schoch <marty.schoch@gmail.com>
2024-09-16 12:16:37 -0400

    fix format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,12 +27,12 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
-          "args": "all",
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_"
-        }
-      ]
+          args: "all",
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
     },
   }
 );
