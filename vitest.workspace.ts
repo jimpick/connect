796d8abe3e971af084ba2f8210f4d4d316f5b9cc
Chris Anderson <jchris@gmail.com>
2024-09-19 12:58:29 -0700

    workspace more

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,8 +1,8 @@
 import { defineWorkspace } from "vitest/config";
 
 import betterSqlite3 from "./vitest.better-sqlite3.config.ts";
-// import nodeSqlite3Wasm from "./vitest.node-sqlite3-wasm.config.ts";
-// import s3 from "./vitest.s3.config.ts";
+import nodeSqlite3Wasm from "./vitest.node-sqlite3-wasm.config.ts";
+import s3 from "./vitest.s3.config.ts";
 import partykit from "./vitest.partykit.config.ts";
 import aws from "./vitest.aws.config.ts";
 // import connector from "./vitest.connector.config.ts";
@@ -13,10 +13,11 @@ void betterSqlite3;
 void netlify;
 void aws;
 void partykit;
+void s3;
 
 export default defineWorkspace([
-  //nodeSqlite3Wasm,
-  // betterSqlite3,
+  nodeSqlite3Wasm,
+  betterSqlite3,
   // connector,
   // s3,
   aws,
