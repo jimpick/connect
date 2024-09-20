5036c606acc7fc2448a68bd2e6148d617c3ebb03
Marty Schoch <marty.schoch@gmail.com>
2024-09-20 09:51:46 -0400

    some plausible index.ts for us to hack on

--- src/partykit/index.ts
+++ src/partykit/index.ts
@@ -5,16 +5,17 @@ import { type Connectable } from "@fireproof/core";
 // Usage:
 //
 // import { useFireproof } from 'use-fireproof'
-// import { connect } from '@fireproof/netlify'
+// import { connect } from '@fireproof/partykit'
 //
 // const { db } = useFireproof('test')
 //
-// const url = URI.from("netlify://localhost:8888").build();
+// const url = URI.from("partykit://localhost:1999).build();
+// url.setParam("protocol", "ws");
 //
-// const cx = connect.netlify(db, url);
+// const cx = connect.partykit(db, url);
 
 export const connect = {
-  netlify: async ({ sthis, blockstore }: Connectable, url?: CoerceURI) => {
+  partykit: async ({ sthis, blockstore }: Connectable, url?: CoerceURI) => {
     const connection = await connectionFactory(sthis, url);
     await connection.connect_X(blockstore);
     //return connection;
