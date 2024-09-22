63ec7a2846d65e76c4dfda596e9a84d2e1e9d58b
Chris Anderson <jchris@gmail.com>
2024-09-22 13:52:28 -0700

    aws connector index

---
+++ src/connect-aws/index.ts
@@ -0,0 +1,43 @@
+import { connectionFactory } from "../connection-from-store";
+import { bs } from "@fireproof/core";
+
+import { registerAWSStoreProtocol } from "./aws-gateway";
+
+// Usage:
+//
+// import { useFireproof } from 'use-fireproof'
+// import { connect } from '@fireproof/aws'
+//
+// const { db } = useFireproof('test')
+//
+// const cx = connect.aws(db);
+
+// TODO need to set the keybag url automatically
+
+// if (!process.env.FP_KEYBAG_URL) {
+//   process.env.FP_KEYBAG_URL = "file://./dist/kb-dir-aws?fs=mem";
+// }
+
+if (!process.env.FP_KEYBAG_URL?.includes("extractKey=_deprecated_internal_api")) {
+  const url = new URL(process.env.FP_KEYBAG_URL || "file://./dist/kb-dir-aws?fs=mem");
+  url.searchParams.set("extractKey", "_deprecated_internal_api");
+  process.env.FP_KEYBAG_URL = url.toString();
+}
+
+registerAWSStoreProtocol();
+
+export const connect = {
+  aws: ({ sthis, blockstore, name }: bs.Connectable, url = "https://aws.amazon.com", region = "us-east-2", uploadUrl = "https://7leodn3dj2.execute-api.us-east-2.amazonaws.com/uploads", webSocketUrl = "wss://fufauby0ii.execute-api.us-east-2.amazonaws.com/Prod", dataUrl = "https://fp1-uploads-201698179963.s3.us-east-2.amazonaws.com") => {
+    const urlObj = new URL(url);
+    urlObj.searchParams.set("name", name || "default");
+    urlObj.searchParams.set("region", region);
+    urlObj.searchParams.set("uploadUrl", uploadUrl);
+    urlObj.searchParams.set("webSocketUrl", webSocketUrl);
+    urlObj.searchParams.set("dataUrl", dataUrl);
+    const fpUrl = urlObj.toString().replace("https", "aws");
+    console.log("Connecting to AWS", fpUrl);
+    const connection = connectionFactory(sthis, fpUrl);
+    connection.connect_X(blockstore);
+    return connection;
+  },
+};
