3418158a8b682fdb0957df926c054f1ddd15e60b
Chris Anderson <jchris@gmail.com>
2024-10-18 17:56:34 -0700

    format

--- setup.cloud.ts
+++ setup.cloud.ts
@@ -2,6 +2,5 @@ import { registerFireproofCloudStoreProtocol } from "./src/cloud/gateway.ts";
 
 registerFireproofCloudStoreProtocol();
 
-process.env.FP_STORAGE_URL =
-  "fireproof://cloud.fireproof.direct?getBaseUrl=https://storage.fireproof.direct/";
+process.env.FP_STORAGE_URL = "fireproof://cloud.fireproof.direct?getBaseUrl=https://storage.fireproof.direct/";
 process.env.FP_KEYBAG_URL = "file://./dist/kb-dir-fireproof-cloud?extractKey=_deprecated_internal_api";
