cacf10c88edd5975c50d5b3f92a4053b462ef60e
Steven Vandevelde <steven@tokono.ma>
2024-10-22 18:36:05 +0200

    chore: Make ucan-cloud connector not a Promise

--- src/ucan-cloud/index.ts
+++ src/ucan-cloud/index.ts
@@ -1,4 +1,4 @@
-import { KeyedResolvOnce } from "@adviser/cement";
+import { KeyedResolvOnce, runtimeFn } from "@adviser/cement";
 import { bs, Database } from "@fireproof/core";
 
 import { connectionFactory, makeKeyBagUrlExtractable } from "../connection-from-store";
@@ -11,15 +11,10 @@ import { registerUCANStoreProtocol } from "./ucan-gateway";
 //
 // const { db } = useFireproof('test')
 //
-// const url = URI.from("ucan://localhost:8787").build();
-// const cx = connect.ucan(db, url);
+// const cx = connect.ucan(db, 'example@email.com', 'http://localhost:8787');
 
-if (
-  typeof process !== "undefined" &&
-  process.env &&
-  !process.env.FP_KEYBAG_URL?.includes("extractKey=_deprecated_internal_api")
-) {
-  const url = new URL(process.env.FP_KEYBAG_URL || "file://./dist/kb-dir-ucan?fs=mem");
+if (!runtimeFn().isBrowser) {
+  const url = new URL(process.env.FP_KEYBAG_URL || "file://./dist/kb-dir-ucan-cloud");
   url.searchParams.set("extractKey", "_deprecated_internal_api");
   process.env.FP_KEYBAG_URL = url.toString();
 }
@@ -30,11 +25,7 @@ registerUCANStoreProtocol();
 
 const connectionCache = new KeyedResolvOnce<bs.Connection>();
 
-export const connect = (
-  db: Database,
-  email: `${string}@${string}`,
-  url = "http://localhost:8787"
-): Promise<bs.Connection> => {
+export const connect = (db: Database, email: `${string}@${string}`, url = "http://localhost:8787"): bs.Connection => {
   const { sthis, blockstore, name: dbName } = db;
 
   if (!dbName) {
@@ -55,7 +46,7 @@ export const connect = (
   const fpUrl = urlObj.toString().replace("http://", "ucan://").replace("https://", "ucan://");
   console.log("fpUrl", fpUrl);
 
-  return connectionCache.get(fpUrl).once(async () => {
+  return connectionCache.get(fpUrl).once(() => {
     makeKeyBagUrlExtractable(sthis);
     console.log("Connecting to Fireproof Cloud", fpUrl);
     const connection = connectionFactory(sthis, fpUrl);
