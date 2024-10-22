5daf44e80c60235eef9a80577e8b134392154683
Steven Vandevelde <steven@tokono.ma>
2024-10-22 16:26:07 +0200

    fix: Attempt to fix promisify bundle issue

--- node-util-polyfill.js
+++ node-util-polyfill.js
@@ -1,4 +1,10 @@
 // The node polyfills we use don't have the `isDeepStrictEqual` method,
 // so we have to get it from elsewhere.
+import { promisify as pr } from "@jspm/core/nodelibs/util";
+
 export * from "@jspm/core/nodelibs/util";
 export { isDeepStrictEqual } from "is-deep-strict-equal-x";
+
+export function promisify(fn) {
+  return pr(fn);
+}
